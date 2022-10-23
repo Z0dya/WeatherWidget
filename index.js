becf6f5a6697822478320bf90c3e546d2ceffd92
Z0dya <vladislavlazutkin64@gmail.com>
2022-10-23 17:42:03 +0300

    add new api

--- index.js
+++ index.js
@@ -6,6 +6,9 @@
 const select = document.querySelector("#select"); //обращаемся к select'y
 addAllOptions()
 
+
+
+
 //функция получения api стран и столиц
 async function getOptions() {
 	try {
@@ -23,6 +26,17 @@ async function getOptions() {
 		alert(error);
 	}
 }
+
+
+// копируем название города
+let globalCapital;
+//вызываем эту функцию каждые 10 секунд
+setInterval(() => {
+	//если город не null, не false, не пустая строка
+	if (globalCapital) {
+		getWeather(globalCapital);
+	}
+}, 10000);
 
 //функция заполнения select'a странами
 async function addAllOptions() {
@@ -47,33 +61,66 @@ async function addAllOptions() {
 			//добавление в select option'ы с странами и столицами
 			select.appendChild(optionNode)
 		}
-
 	}
 }
 
 //? событие выбора страны
 select.addEventListener('change', () => {
 	//? отправляем в виджет выбранную страну(столицу)
+	globalCapital = select.value;
 	getWeather(select.value)
 })
 
+
+
 //функция получения api по столице и заполнения виджета данными о погоде
 async function getWeather(capital) {
+	// переменные для изменения фона виджета
+	let Widget;
+	let tempCount;
+
+	// Встроенный объект который юзаем для отмены fetch
+	let controller = new AbortController();
+
+	// функция которая через 5 сек вызывает прерывание запроса
+	setTimeout(() => {
+		controller.abort()
+	}, 5000)
+
 	try {
-		//получение api
-		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=39be4fcbdf5882d26890b1fcbae75d12`)
-		//преобразование полученных данных в объект
-		const data = await response.json();
+		try {
+			//получение api
+			const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=fb876a71a8844bf7dbd7aa9a8080a2e9`, { signal: controller.signal }) //передаем сигнал (сигнал: значение сигнала)
+			//преобразование полученных данных в объект
+			const data = await response.json();
+			console.log(data);
+			//? присваивание HTML тегов к полученным объектам после получения данных
+			Widget = document.querySelector('.weatherBlock');
+			document.querySelector('.cityName').textContent = data.name;
+			tempCount = document.querySelector('.temp__count').textContent = Math.round(data.main.temp - 273);
+			document.querySelector('.temp__description').textContent = data.weather[0].description;
+			document.querySelector('.weather__icon').innerHTML = `<img src = ./img/${data.weather[0].icon}@2x.png>`;
+			document.querySelector('.temp__wind').innerHTML = data.wind.speed + ' м/c';
+		}
 
-		//? присваивание HTML тегов к полученным объектам после получения данных
-		let Widget = document.querySelector('.weatherBlock');
-		document.querySelector('.cityName').textContent = data.name;
-		let tempCount = document.querySelector('.temp__count').textContent = Math.round(data.main.temp - 273);
-		document.querySelector('.temp__description').textContent = data.weather[0].description;
-		document.querySelector('.weather__icon').innerHTML = `<img src = ./img/${data.weather[0].icon}@2x.png>`;
-		document.querySelector('.temp__wind').innerHTML = data.wind.speed + ' м/c';
+		// если не выполняется запрос с 1 API то выполняем запрос с другой
+		catch {
+			const response = await fetch(`https://goweather.herokuapp.com/weather/${capital}`);
+			const data = await response.json();
 
-		console.log(tempCount);
+			//? присваивание HTML тегов к полученным объектам после получения данных
+			Widget = document.querySelector('.weatherBlock');
+			document.querySelector('.cityName').textContent = capital;
+
+			var numberPattern = /\d+/g;
+
+			tempCount = document.querySelector('.temp__count').textContent = Math.round(data.temperature.match(numberPattern));
+			document.querySelector('.temp__description').textContent = data.description;
+			document.querySelector('.weather__icon').innerHTML = `<img src = ./img/temperatureicon.png>`;
+			let newWind = Math.round(data.wind.match(numberPattern) / 3.6);
+			document.querySelector('.temp__wind').innerHTML = newWind + ' м/c';
+		}
+
 		//?изменение фона виджета в зависимости от температуры
 		if (tempCount > 0 && tempCount <= 20) {
 			Widget.classList.add('normal');
